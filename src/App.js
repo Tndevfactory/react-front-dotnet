bed390b221307252b85c76e1a7657bad77304190
Tndevfactory <tndevfactory@gmail.com>
2022-03-31 19:06:59 +0100

    f

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { TndevCtx } from "./contexts/TndevContext";
+import Button from "@mui/material/Button";
+import Navbare from "./components/Navbare";
 
 function App() {
+  const [methods, states] = TndevCtx();
+  const { authMethods } = methods;
+  const { testContext } = authMethods;
+  const { color } = states;
+
+  const handleClick = () => {
+    let t = testContext();
+    console.log(t);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbare />
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { TndevProvider } from "./contexts/TndevContext";
 
-ReactDOM.render(
+const queryClient = new QueryClient();
+
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <QueryClientProvider client={queryClient}>
+//       <TndevProvider>
+//         <App />
+//       </TndevProvider>
+//     </QueryClientProvider>
+//   </React.StrictMode>,
+//   document.getElementById("root")
+// );
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <QueryClientProvider client={queryClient}>
+      <TndevProvider>
+        <App />
+      </TndevProvider>
+    </QueryClientProvider>
+  </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
